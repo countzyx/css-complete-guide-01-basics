8376adb1383c99d4d27ba5919fc79d9807668d0d
Pedro R. Jiménez <countzyx@gmail.com>
2021-07-15 03:43:39 -0400

    Added Event Handler for Toggle Button and Mobile Menu

--- 01-basics/shared.js
+++ 01-basics/shared.js
@@ -1,15 +1,28 @@
 $(function () {
+  const toggleButton = $('#toggle-button');
   const backdrop = $('#backdrop');
+  const mobileNav = $('#mobile-nav');
+  toggleButton.click(function () {
+    backdrop.show();
+    mobileNav.show();
+  });
+
   const modal = $('#modal');
   // console.log(backdrop, modal);
   const closeModal = function () {
     modal.hide();
     backdrop.hide();
   };
-  backdrop.click(closeModal);
+
+  backdrop.click(function () {
+    closeModal();
+    mobileNav.hide();
+  });
+
   const noButton = modal.find('.modal__action--negative');
   // console.log(noButton);
   noButton.click(closeModal);
+
   const planButtons = $('.plan button');
   // console.log(planButtons);
   planButtons.click(function () {
